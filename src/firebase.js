f07cdc80fe041d07bc70f82f48979562fd53584b
junseinagao <54606780+junseinagao@users.noreply.github.com>
2021-05-28 15:37:06 +0900

    clear firebase js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,6 @@
 import firebase from "firebase"
 import "firebase/firestore"
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDyH1MTCug_VBu6s-ALNVun6MRo_M7dizE",
-  authDomain: "test-app-bfb71.firebaseapp.com",
-  projectId: "test-app-bfb71",
-  storageBucket: "test-app-bfb71.appspot.com",
-  messagingSenderId: "860235749211",
-  appId: "1:860235749211:web:c4e5e25b54da3438136c7e",
-}
+const firebaseConfig = {}
 
 firebase.initializeApp(firebaseConfig)
