6b3239f0492f4d6496707dc98d261fcaa679b2d6
junseinagao <junsei.nagao@gmail.com>
2021-05-30 12:30:19 +0900

    App.vueを更新

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div id="app">
     <div><QiitaAPI /></div>
+    <div><Firebase /></div>
+    <div><ExampleApp /></div>
   </div>
 </template>
 
 <script>
 import QiitaAPI from "@/components/QiitaAPI.vue"
+import Firebase from "@/components/Firebase.vue"
+import ExampleApp from "@/components/ExampleApp.vue"
 export default {
   name: "App",
   components: {
     QiitaAPI,
+    Firebase,
+    ExampleApp,
   },
 }
 </script>
